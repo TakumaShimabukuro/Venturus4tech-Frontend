10398739d069519243b1136dd050cb6b265d5cc5
Takuma Shimabukuro <ltakumal.20.3@gmail.com>
2020-01-27 13:07:19 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Login from "./containers/login/login"
+
+function App() {
+  return (
+    <div className="App">
+      <Login name="Takuma" age="20"/>
+    </div>
+  );
+}
+
+export default App;
